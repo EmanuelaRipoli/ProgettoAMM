8c18730f9bc0e9846d8f14742aa545730bb04134
emanuelaripoli <stsad@tiscali.it>
2016-07-16 18:21:29 +0200

    aggiunta index

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+
+		<h1>Accesso all'applicazione del progetto</h1>
+		<p>E' possibile scaricare il codice direttamente dal web o facendo un git clone al seguente indirizzo <a href="https://github.com/EmanuelaRipoli/ProgettoAMM">https://github.com/EmanuelaRipoli/ProgettoAMM</a> </br></p>
+        
+        <h2>Descrizione del progetto</h2>
+        <p> L'applicazione permette di ordinare libri, ebook, cd musicali, film e giochi.
+			</br>	
+            La funzionali&agrave; di base prevede che un acquirente possa effettuare un ordine con i relativi prodotti desiderati
+            e visualizzare gli ordini effettuati. 
+			</br>
+            L'amministratore potr&agrave; cancellare e ricercare i vari ordini presenti. 
+			</br>
+            I dati che figurano per ogni libro e ebook sono i seguenti:
+        </p>
+        <ul>
+            <li>Autore</li>
+            <li>Titolo</li>
+            <li>Codice</li>
+            <li>Prezzo</li>
+            <li>Genere</li>
+            <li>Editore</li>
+            <li>Data di pubblicazione</li>
+            <li>Formato</li>
+            <li>Lingua</li>
+            <li>Descrizione</li>
+        </ul>
+        <p>I dati che figurano per ogni cd musicale sono i seguenti: </p>
+         <ul>
+            <li>Cantante</li>
+            <li>Titolo</li>
+            <li>Codice</li>
+            <li>Prezzo</li>
+            <li>Genere</li>
+            <li>Casa discografica</li>
+            <li>Data di pubblicazione</li>
+            <li>Formato</li>
+            <li>Descrizione</li>
+        </ul>
+        <p>I dati che figurano per ogni film sono i seguenti: </p>
+         <ul>
+            <li>Regista</li>
+            <li>Titolo</li>
+            <li>Codice</li>
+            <li>Prezzo</li>
+            <li>Genere</li>
+            <li>Editore</li>
+            <li>Data di pubblicazione</li>
+            <li>Formato</li>
+            <li>Durata</li>
+            <li>Descrizione</li>
+        </ul>
+        <p>I dati che figurano per ogni gioco sono i seguenti: </p>
+         <ul>
+            <li>Titolo</li>
+            <li>Codice</li>
+            <li>Prezzo</li>
+            <li>Genere</li>
+            <li>Editore</li>
+            <li>Data di pubblicazione</li>
+            <li>Formato</li>
+            <li>Pegi</li>
+            <li>Descrizione</li>
+        </ul>
+        
+        
+    </ul>
+
+    <h2>Accesso al progetto</h2>
+    <p>Link per accesso al progetto:<a href="http://spano.sc.unica.it/amm2015/ripoliEmanuela/servizi/libri.php">http://spano.sc.unica.it/amm2015/ripoliEmanuela/servizi/libri.php</a></br></p>
+    <p>Si pu&ograve; accedere alla applicazione con le seguenti credenziali</p>
+    
+	<p><strong>Ruolo utente:</strong></p>
+	<ul>
+		<li>username: utente</li>
+		<li>password: utente</li>
+	</ul>
+	<p><strong>Ruolo amministratore:</strong></p>
+	<ul>
+		<li>username: amministratore</li>
+		<li>password: amministratore</li>
+	</ul>
+
+</body>
+</html>
